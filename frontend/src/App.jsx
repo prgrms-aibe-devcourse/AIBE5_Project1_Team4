cd1eb8f9fdc91a8d56b83f542da8b75e1b102a76
seoilhyeon <ilhyeon0326@gmail.com>
2026-01-20 19:32:21 +0900

    [Feat] #1 - 프리뷰 페이지 작업 완료
    * 프리뷰 페이지
    * 라우터 설정 - 프리뷰 페이지가 dev환경에서만 보이게 설정
    * 부트스트랩 스타일 적용

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,19 @@
 // import './App.css';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import UiPreview from './pages/UiPreview';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        {/* 개발용 UI 쇼룸 */}
+        {import.meta.env.DEV && (
+          <Route path="/preview" element={<UiPreview />} />
+        )}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
