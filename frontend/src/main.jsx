2149b923b3d7d740f458e833f2625be3e1a4e24b
seoilhyeon <ilhyeon0326@gmail.com>
2026-01-22 22:06:39 +0900

    [Feat] #17 - 인증 스토어 앱에 적용

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import { AuthProvider } from './context/AuthContext.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './features/auth/components/AuthProvider.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
   </StrictMode>,
 );
