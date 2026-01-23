2877df2bdd7103f51a336d927233cd522fa5efb8
DoubleJ1039 <evan101637@gmail.com>
2026-01-23 17:34:32 +0900

    SearchBar 컴포넌트 파일과 SearchBar UI구현

---
+++ frontend/src/ui-preview/sections/SearchBar.preview.jsx
@@ -0,0 +1,11 @@
+import SearchBar from '../../components/SearchBar';
+
+const SearchBarPreview = () => {
+  return (
+    <div style={{ padding: '20px' }}>
+      <SearchBar />
+    </div>
+  );
+};
+
+export default SearchBarPreview;
