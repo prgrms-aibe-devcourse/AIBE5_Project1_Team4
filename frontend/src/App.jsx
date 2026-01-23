dc882aa758b938ccf87703a94314edb76accdee4
jhwna1205-sys <jhwan1205@gmail.com>
2026-01-23 11:17:15 +0900

    [Feat] #27 랜딩 페이지 UI 구현

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,26 @@
 // import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from 'react-router-dom';
+
+
+// 페이지 컴포넌트 import
+import HomePage from './pages/HomePage';
+
+// 기존 개발용 컴포넌트
 import AuthCallbackPage from './pages/AuthCallbackPage';
 import Health from './pages/Health';
 import UiPreview from './pages/UiPreview';
 
 function App() {
   return (
-    <div>
-      <Routes>
-        {/* 개발용 UI 쇼룸 */}
-        {import.meta.env.DEV && (
+   <div>
+        <Routes>
+          {/* 1. 메인 기능 라우트 */}
+          <Route path="/" element={<HomePage />} />
+
+
+          {/* 개발용 UI 쇼룸 */}
+          {import.meta.env.DEV && (
           <Route path="/preview" element={<UiPreview />} />
         )}
         {import.meta.env.DEV && <Route path="/health" element={<Health />} />}
