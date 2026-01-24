b21a3154c067cd40e1634a24189eeeea158559ab
seoilhyeon <ilhyeon0326@gmail.com>
2026-01-24 22:59:44 +0900

    [Fix] #74 - 인덱스 컬럼 수정

--- supabase/migrations/20260124000009_index.sql
+++ supabase/migrations/20260124000009_index.sql
@@ -3,4 +3,4 @@ create index if not exists idx_trip_bookmarks_trip_user on public.trip_bookmarks
 create index if not exists idx_trip_members_trip_user on public.trip_members(trip_id, user_id);
 
 create index if not exists idx_trip_days_trip_date on public.trip_days(trip_id, date);
-create index if not exists idx_schedule_items_day_time on public.schedule_items(day_id, time, order_index);
+create index if not exists idx_schedule_items_day_time on public.schedule_items(trip_day_id, time, order_index);
