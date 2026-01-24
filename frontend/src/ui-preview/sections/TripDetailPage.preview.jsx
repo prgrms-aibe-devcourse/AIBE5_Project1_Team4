4e45cd7168befcc593580aa3e518de95dbd579b2
jhwna1205-sys <jhwan1205@gmail.com>
2026-01-25 08:31:03 +0900

    [Feat] # 71 -  컴포넌트 주석수정

--- frontend/src/ui-preview/sections/TripDetailPage.preview.jsx
+++ frontend/src/ui-preview/sections/TripDetailPage.preview.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import TripDetailPage from '../../pages/TripDetailPage';
 
-// (선택사항) 메타 정보 - 나중에 다시 리스트 형태가 필요할 때를 대비해 남겨둠
+
 export const meta = {
   title: '여행 상세 페이지 통합 프리뷰',
   order: 1,
