df4d94996d42afa9cd10055aac14688cbab9673b
jhwna1205-sys <jhwan1205@gmail.com>
2026-01-26 15:55:32 +0900

    [Fix] #99 - 지도 섹션 높이 단위 수정(px) 및 일정 리스트 컴포넌트 복구

--- frontend/src/components/trip-detail/TripMapSection.jsx
+++ frontend/src/components/trip-detail/TripMapSection.jsx
@@ -8,10 +8,13 @@ const TripMapSection = () => {
   // 지금은 '데이터가 없을 때 버튼이 잘 나오는지' 확인해야 하므로 빈 상태로 둡니다.
 
   return (
-    <div className="trip-detail-map-container" style={{ position: 'relative', width: '100%', height: '100%' }}>
+    <div className="trip-detail-map-container" style={{ 
+      position: 'relative', 
+      width: '100%', 
+      height: '600px' 
+    }}>
       
       {/* 2. 여기에 리스트 컴포넌트를 꼭 넣어줘야 화면에 나옵니다! */}
-      {/* 데이터가 없어도(props를 안 줘도) 버튼은 보여야 합니다. */}
       <TripItineraryList />
       
       {/* 줌 컨트롤 */}
